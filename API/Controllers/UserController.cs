fb783f45ced5196e13b1cd04b1b44c5440583156
Mark James Magante <mj@bizbox.ph>
2023-06-08 21:22:42 +0800

    End of section 12

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -89,7 +89,9 @@ namespace API.Controllers
 
 
             var currentMain = user.Photos.FirstOrDefault(x => x.IsMain);
-            if(currentMain != null) currentMain.IsMain = false;
+            if(currentMain != null) 
+               currentMain.IsMain = false;
+
             photo.IsMain = true;
 
             if(await _userRepository.SaveAllAsync()) return NoContent();
--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -78,9 +78,15 @@ export class PhotoEditorComponent implements OnInit {
     }
 
     this.uploader.onSuccessItem = (item, response, status, headers) => {
-      if(response){
+      if(response ){
         const photo = JSON.parse(response);
         this.member?.photos.push(photo);
+        if(photo.isMain && this.member && this.user){
+          this.member.photoUrl = photo.url;
+          this.user.photoUrl = photo.url;
+          this.accountService.setCurrentUser(this.user);
+        }
+
       }
     }
   
